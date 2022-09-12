b84f421de3ecf827554b628a4905bd170148223e
Huzg1st <vfvvvvv22@gmail.com>
2022-09-13 02:38:42 +0800

    first cmmit

---
+++ lnmp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#========================================================
+#   System Required: CentOS 7+ / Debian 8+ / Ubuntu 16+ /
+#     Arch 未测试
+#   Description: lnmp安装
+#   Github: https://github.com/Huzg1st/lnmp-install
+#========================================================
+
+等待
